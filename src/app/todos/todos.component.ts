83d6d753876be4b02e229f78a5081d58e91ed146
bristy1411 <mahbuba.bristy14@gmail.com>
2025-02-13 02:39:49 +0600

    services and http call

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { TodosService } from '../services/todos.service';
+import { Todo } from '../model/todo.type';
+import { catchError } from 'rxjs';
 
 @Component({
   selector: 'app-todos',
   standalone: true,
   imports: [],
   templateUrl: './todos.component.html',
-  styleUrl: './todos.component.scss'
+  styleUrl: './todos.component.scss',
 })
-export class TodosComponent {
+export class TodosComponent implements OnInit {
+  todoService = inject(TodosService);
+  todoItems = signal<Array<Todo>>([]);
 
+  ngOnInit(): void {
+    // this.todoItems.set(this.todoService.todoItem);
+    // console.log(this.todoService.todoItem);
+    this.todoService
+      .getTodosFromApi()
+      .pipe(
+        catchError((err) => {
+          console.log(err);
+          throw err;
+        })
+      )
+      .subscribe((todos) => {
+        this.todoItems.set(todos);
+      });
+  }
 }
